ca20478f004ed29ae6975fbedfa46483119904a9
Vladyslav Peresta <perestavlad@gmail.com>
2022-11-06 23:15:18 +0200

    Corrected debug settings

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "py-taxi-service-deploying.herokuapp.com"]
 
